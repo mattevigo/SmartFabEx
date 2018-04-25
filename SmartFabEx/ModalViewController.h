6c7bc905d6242f195bc51a2e76c0b29358c6894b
Matteo Vigoni <mattevigo@gmail.com>
2018-04-25 17:13:43 +0200

    ModalViewController

---
+++ SmartFabEx/ModalViewController.h
@@ -0,0 +1,13 @@
+//
+//  ModalViewController.h
+//  SmartFabEx
+//
+//  Created by Matteo Vigoni on 25/04/2018.
+//  Copyright © 2018 Odd Jobs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ModalViewController : UIViewController
+
+@end
---
+++ SmartFabEx/ModalViewController.m
@@ -0,0 +1,37 @@
+//
+//  ModalViewController.m
+//  SmartFabEx
+//
+//  Created by Matteo Vigoni on 25/04/2018.
+//  Copyright © 2018 Odd Jobs. All rights reserved.
+//
+
+#import "ModalViewController.h"
+
+@interface ModalViewController ()
+
+@end
+
+@implementation ModalViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
